244b952843a5ec0dff3c7f259a6dd1dd72b59c1b
MaxHalford <maxhalford25@gmail.com>
2016-08-17 23:28:27 +0200

    Vendor the leaflet images folder

--- src/main.js
+++ src/main.js
@@ -3,13 +3,9 @@ import 'expose?$!jquery';
 import 'expose?jQuery!jquery';
 
 import 'materialize-css';
-import 'leaflet';
 
 import App from './App';
 import router from './router';
 
-router.beforeEach(() => {
-  window.scrollTo(0, 0);
-});
-
+router.beforeEach(() => { window.scrollTo(0, 0); });
 router.start(App, '#app');
--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -3,12 +3,16 @@
 </template>
 
 <script>
+import 'leaflet';
+
 export default {
   name: 'Map',
   data: () => ({
     map: null,
   }),
   ready: function ready() {
+    L.Icon.Default.imagePath = '/static/vendor/leaflet/images';
+
     this.map = L.map('map');
     L.tileLayer('https://{s}.tiles.mapbox.com/v4/{mapId}/{z}/{x}/{y}.png?access_token={token}', {
       attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a>',
@@ -16,7 +20,10 @@ export default {
       mapId: 'mapbox.outdoors',
       token: 'pk.eyJ1IjoibGVtYXgiLCJhIjoidnNDV1kzNCJ9.iH26jLhEuimYd6vLOO6v1g',
     }).addTo(this.map);
-    this.map.setView([51.505, -0.09], 13);
+    this.map.setView([51.505, -0.09], 12);
+    L.marker([51.5, -0.09]).addTo(this.map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
   },
 };
 </script>
