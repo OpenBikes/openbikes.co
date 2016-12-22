82d43a4c998029e02ce2c765a6faca1aebb324dc
MaxHalford <maxhalford25@gmail.com>
2016-12-22 20:40:04 +0100

    Initial dist subtree commit

--- src/store/api.js
+++
@@ -1,5 +0,0 @@
-import { fetchJson } from './util'
-
-export const fetchCities = () => fetchJson('GET', '/api/cities')
-export const fetchCity = (citySlug) => fetchJson('GET', `/api/cities?city_slug=${citySlug}`)
-export const fetchStations = (citySlug) => fetchJson('GET', `/api/geojson/${citySlug}`)
